734d0a778e2b554389ba1cdc695760232e0f8c43
Rajesh Rout <rajeshrauta5@gmail.com>
2024-03-14 00:16:57 +0530

    Added hangfire jobs

--- src/app/pages/accounting/accounting.module.ts
+++ src/app/pages/accounting/accounting.module.ts
@@ -6,9 +6,18 @@ import { AccountingRoutingModule } from './accounting-routing.module';
 import { AllOverdueComponent } from './all-overdue/all-overdue.component';
 import { TodayOverdueComponent } from './today-overdue/today-overdue.component';
 import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AllOverdueComponent, TodayOverdueComponent],
-  imports: [CommonModule, AccountingRoutingModule, UIModule, BsDropdownModule],
+  imports: [
+    CommonModule, 
+    AccountingRoutingModule, 
+    UIModule, 
+    BsDropdownModule,
+    ModalModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AccountingModule {}
--- src/app/pages/setting/hangfire-jobs/hangfire-jobs.component.ts
+++ src/app/pages/setting/hangfire-jobs/hangfire-jobs.component.ts
@@ -14,4 +14,32 @@ export class HangfireJobsComponent {
       { label: "Hangfire Jobs", active: true },
     ];
   }
+
+
+  hangFireJobs = [
+    {
+      id: "HJ0001",
+      jobName: "Update customer status to deleted",
+    },
+    {
+      id: "HJ0002",
+      jobName: "Sync all entities from begining",
+    },
+    {
+      id: "HJ0003",
+      jobName: "Update customer open and overdue balance",
+    },
+    {
+      id: "HJ0004",
+      jobName: "Another hangfire job",
+    },
+    {
+      id: "HJ0005",
+      jobName: "Another hangfire job",
+    },
+    {
+      id: "HJ0006",
+      jobName: "Another hangfire job",
+    },
+    ]
 }
