bb741c9899e948b1433e316b985de6d333899ed0
Rajesh Rout <rajeshrauta5@gmail.com>
2024-03-28 01:46:06 +0530

    modal edited

--- src/app/shared/modals/estimate-detail/estimate-detail.component.ts
+++ src/app/shared/modals/estimate-detail/estimate-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-estimate-detail',
@@ -8,6 +9,16 @@ import { Component, ViewChild } from '@angular/core';
 export class EstimateDetailComponent {
   @ViewChild('estimateDetailModal') estimateDetailModal: any; // Assuming 'invoiceDetailModal' is the template reference variable for the modal
 
+  constructor(private router: Router) { }
+
+
+  navigateToEditInTab() {
+    this.router.navigate(['/estimate', 'edit-estimate']);
+  }
+
+  navigateToEditInManual() {
+    this.router.navigate(['/estimate', 'edit-manual-estimate']);
+  }
   showModal() {
     this.estimateDetailModal.show();
   }
--- src/app/shared/modals/invoice-detail/invoice-detail.component.ts
+++ src/app/shared/modals/invoice-detail/invoice-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component,ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-invoice-detail',
@@ -8,6 +9,15 @@ import { Component,ViewChild } from '@angular/core';
 export class InvoiceDetailComponent {
   @ViewChild('invoiceDetailModal') invoiceDetailModal: any; // Assuming 'invoiceDetailModal' is the template reference variable for the modal
 
+  constructor(private router: Router) { }
+
+  navigateToEditInvoice() {
+    this.router.navigate(['/invoice', 'edit']);
+  }
+
+  navigateTolinkedwithestimate() {
+    this.router.navigate(['/invoice', 'invoice-linked-with-estimate']);
+  }
   showModal() {
     this.invoiceDetailModal.show();
   }
